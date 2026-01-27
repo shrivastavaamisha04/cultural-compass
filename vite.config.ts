3ee0ccd5fa15ac7a9ac1fb37b5ce01eeb139d1a5
Amisha Shrivastava <amishashrivastava@Amishas-MacBook-Air.local>
2026-01-27 19:58:03 +0800

    Fix ESM __dirname error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
